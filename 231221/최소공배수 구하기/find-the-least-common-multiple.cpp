a65049010f1d988d39108d820aa34bb94e605ce0
Yoon Hyuck Cho <77561827+cchonac@users.noreply.github.com>
2023-12-21 18:51:01 +0900

    Title: 최소공배수 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231221/최소공배수 구하기/find-the-least-common-multiple.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int gcd(int a,int b){
+        int r;
+        while(b!=0){
+            r=a%b;
+            a=b;
+            b=r;
+        }
+    
+    return a;
+}
+
+int main() {
+    int a,b;
+    cin>>a>>b;
+
+    cout<<(a*b)/gcd(a,b);
+ 
+    return 0;
+}
