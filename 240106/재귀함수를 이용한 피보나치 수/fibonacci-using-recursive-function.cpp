55b144610f7f8a5fdb3f52b9b32c458f8d3be537
Yoon Hyuck Cho <77561827+cchonac@users.noreply.github.com>
2024-01-06 11:50:42 +0900

    Title: 재귀함수를 이용한 피보나치 수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240106/재귀함수를 이용한 피보나치 수/fibonacci-using-recursive-function.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int Fib(int n){
+    if(n==1||n==2){
+        return 1;
+    }else{
+        return Fib(n-1)+Fib(n-2);
+    }
+}
+
+int main() {
+    int n;
+    cin>>n;
+    cout<<Fib(n);
+    return 0;
+}
