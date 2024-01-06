dd4cb1596335c0e3572d96185fed7094d9c590a4
Yoon Hyuck Cho <77561827+cchonac@users.noreply.github.com>
2024-01-07 00:08:32 +0900

    Title: 재귀함수를 이용한 최댓값, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240107/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+
+int Max(int x,int y){
+    if(x>y){
+        return x;
+    }else{
+        return y;
+    }
+}
+
+
+int MaxArray(int len, int arr[]){
+    if(len == 1){
+        return arr[0];
+    }else{
+        return Max(arr[len-1],MaxArray(len-1,arr));
+    }
+}
+
+int main() {
+    int n;
+    cin>>n;
+    int* arr = new int[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    cout<<MaxArray(n,arr);
+    return 0;
+}
