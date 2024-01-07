88674c0541c62e12ba30672ebf8bb2f8d5d1db05
Yoon Hyuck Cho <77561827+cchonac@users.noreply.github.com>
2024-01-07 22:15:46 +0900

    Title: 두 개의 동일한 수열, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240107/두 개의 동일한 수열/two-equal-series.cpp
+++ 240107/두 개의 동일한 수열/two-equal-series.cpp
@@ -15,12 +15,17 @@ int main() {
     }
     sort(arr1,arr1+n);
     sort(arr2,arr2+n);
+    int cnt=0;
     for(int i=0;i<n;i++){
-        if(arr1[n]!=arr2[n]){
-            cout<<"No";
-            break;
+        if(arr1[n]==arr2[n]){
+            cnt++;
         }
     }
-    cout<<"Yes";
+    if(cnt==n){
+        cout<<"Yes";
+    }else{
+        cout<<"No";
+    }
+    
     return 0;
 }
