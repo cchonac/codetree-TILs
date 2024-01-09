902bf9b648b1747d7d7b70af434049fedcce78e1
Yoon Hyuck Cho <77561827+cchonac@users.noreply.github.com>
2024-01-09 22:36:21 +0900

    Title: 비오는 날, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240109/비오는 날/rainy-day.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    class Whether{
+        public: string when;
+                string day;
+                string wh;
+        Whether(string when="",string day="",string wh=""){
+            this->when=when;
+            this->day=day;
+            this->wh=wh;
+        }
+
+    };
+
+    int n;
+    cin>>n;
+    Whether A[n];
+
+    string when;
+    string day;
+    string wh;
+
+    int idx=0;
+    for(int i=0;i<n;i++){
+        cin>>when>>day>>wh;
+        A[i]=Whether(when,day,wh);
+        if(wh=="Rain"){
+            if(idx==0){
+                idx=i;
+            }
+        }
+
+    }
+    cout<<A[idx].when<<" "<<A[idx].day<<" "<<A[idx].wh;
+
+    return 0;
+}
