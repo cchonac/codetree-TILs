686fff6ad936fba653f0a73021e1bfd153192961
Yoon Hyuck Cho <77561827+cchonac@users.noreply.github.com>
2024-01-12 22:50:41 +0900

    Title: 연속되는 수 2, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240112/연속되는 수 2/continuous-number2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+    int n;
+    cin>>n;
+    int cnt=0;
+
+    int currentCount, currentNum, beforeNum;
+    cin>>beforeNum;
+    currentCount=1;
+
+
+
+    for(int i=1;i<n;i++){
+        cin>>currentNum;
+        if(beforeNum==currentNum){
+            currentCount++;
+            
+        }else{
+            currentCount=1;
+        }
+        cnt=max(cnt,currentCount);
+        beforeNum=currentNum;
+    }
+    cout<<cnt;
+    return 0;
+}
