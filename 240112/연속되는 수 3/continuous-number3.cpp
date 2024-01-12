d27e46fbbdc8ef27dabc991f98d6c60583c70d61
Yoon Hyuck Cho <77561827+cchonac@users.noreply.github.com>
2024-01-12 23:18:06 +0900

    Title: 연속되는 수 3, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240112/연속되는 수 3/continuous-number3.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    int beforeNum, currentNum;
+    int cnt=1;
+    int maxcnt=0;
+    cin>>beforeNum;
+    for(int i=1;i<n;i++){
+            cin>>currentNum;
+            if(currentNum*beforeNum>0){
+                cnt++;
+            }else{
+                cnt=1;
+            }
+        maxcnt=max(cnt,maxcnt);
+        beforeNum=currentNum;    
+    }
+    cout<<maxcnt;
+    
+
+    
+    return 0;
+}
