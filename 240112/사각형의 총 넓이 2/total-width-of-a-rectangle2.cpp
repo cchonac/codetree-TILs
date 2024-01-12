338e6bbb553938432f8fc55ee51d0acfc7da9f21
Yoon Hyuck Cho <77561827+cchonac@users.noreply.github.com>
2024-01-12 20:51:56 +0900

    Title: 사각형의 총 넓이 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240112/사각형의 총 넓이 2/total-width-of-a-rectangle2.cpp
+++ 240112/사각형의 총 넓이 2/total-width-of-a-rectangle2.cpp
@@ -5,13 +5,13 @@ int main() {
     int n;
     cin>>n;
     int x1,x2,y1,y2;
-    x1+=100;
-    x2+=100;
-    y1+=100;
-    y2+=100;
-    int arr[101][101]={0,};
+    int arr[201][201]={0,};
     for(int i=0;i<n;i++){
         cin>>x1>>y1>>x2>>y2;
+        x1+=100;
+        x2+=100;
+        y1+=100;
+        y2+=100;
         for(int j=x1;j<x2;j++){
           for(int h=y1;h<y2;h++){
             if(arr[j][h]==0){
@@ -21,8 +21,8 @@ int main() {
         }
     }
     int cnt=0;
-    for(int i=0;i<101;i++){
-        for(int j=0;j<101;j++){
+    for(int i=0;i<201;i++){
+        for(int j=0;j<201;j++){
             if(arr[i][j]>0){
                 cnt++;
             }
