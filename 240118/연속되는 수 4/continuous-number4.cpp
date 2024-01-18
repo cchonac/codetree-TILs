06550fc644c266b97d2da359109c7694b2b37201
Yoon Hyuck Cho <77561827+cchonac@users.noreply.github.com>
2024-01-18 16:33:39 +0900

    Title: 연속되는 수 4, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240118/연속되는 수 4/continuous-number4.cpp
+++ 240118/연속되는 수 4/continuous-number4.cpp
@@ -8,20 +8,17 @@ int main() {
     for(int i=0;i<N;i++){
         cin>>arr[i];
     }
-    int cnt=1;
+    int cnt=0;
     int sol=0;
-    for(int i=0;i<N-1;i++){
+    for(int i=0;i<N;i++){
 
-        if(arr[i]<arr[i+1]){
+        if(i>=1&&arr[i]>arr[i-1]){
             cnt++;
         }else{
             cnt=1;
         }
         sol=max(sol,cnt);
     }
-    if(N==2){
-            sol=1;
-        }
     cout<<sol;
     return 0;
 }
