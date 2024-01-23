cbeb93b83ad5d70a1128344b5a6698599c304086
Yoon Hyuck Cho <77561827+cchonac@users.noreply.github.com>
2024-01-23 21:26:54 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240123/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.cpp
+++ 240123/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.cpp
@@ -26,16 +26,16 @@ bool CanGo(int x, int y){
 }
 
 void DFS(int x, int y){
-    int dx[2]={1,0};
-    int dy[2]={0,1};
+    int dx[2]={0,1};
+    int dy[2]={1,0};
     for(int i=0;i<2;i++){
         int new_x=x+dx[i];
         int new_y=y+dy[i];
 
         if(CanGo(new_x,new_y)){
-        graph[new_x][new_y]++;
-        visited[new_x][new_y]=true;
-        DFS(new_x,new_y);
+          graph[new_x][new_y]++;
+          visited[new_x][new_y]=true;
+          DFS(new_x,new_y);
     }
     }
     
