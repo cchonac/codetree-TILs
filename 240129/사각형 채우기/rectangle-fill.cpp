4283e043deeae9407345adb675531cf38e7e7eb5
Yoon Hyuck Cho <77561827+cchonac@users.noreply.github.com>
2024-01-29 18:43:01 +0900

    Title: 사각형 채우기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240129/사각형 채우기/rectangle-fill.cpp
+++ 240129/사각형 채우기/rectangle-fill.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int dp[1001]={0,};
@@ -8,7 +9,7 @@ int DP(int n){
     dp[0]=1;
     dp[1]=1; 
     for(int i=2;i<=n;i++){
-        dp[i]=dp[i-1]+dp[i-2];
+        dp[i]=(dp[i-1]+dp[i-2])%(10007);
     }
     return dp[n];
 }
