b8514754f2a3d2e7caf69cb39b0398e405170863
Yoon Hyuck Cho <77561827+cchonac@users.noreply.github.com>
2024-02-07 20:23:42 +0900

    Title: 퀵 정렬 구현, Time: 53ms, Memory: 1MB, Status: Passed - CodeTree

--- 240207/퀵 정렬 구현/implement-quick-sort.cpp
+++ 240207/퀵 정렬 구현/implement-quick-sort.cpp
@@ -31,7 +31,7 @@ void quick_sort(int arr[], int low, int high){
         int pi=partition(arr,low,high);
 
         quick_sort(arr,low,pi-1);
-        quick_sort(arr,pi,high);
+        quick_sort(arr,pi+1,high);
     }
 }
 
